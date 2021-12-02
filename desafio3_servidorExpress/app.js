b8321c884d30c2711c1897c60c02cdaff5de8873
Vogeln7 <david.vogel.7@gmail.com>
2021-12-01 22:20:36 -0300

    Commit desafio 3 servidor Express

---
+++ desafio3_servidorExpress/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const ProductContainer = require('./contenedor');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const products= new ProductContainer('products.json');
+products.init()
+
+
+
+app.get('/productos', async(req, res)=>{
+    
+    res.send(await products.getAll())
+    
+})
+
+app.get('/productosRandom',async(req, res)=>{
+
+    let randomId=(min,max)=>{
+        return Math.random()*(max-min)+min;
+    }
+    let idRandom=parseInt(randomId(0,products.productList.length-1))    
+    res.send(await products.getByID(idRandom));
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server Running in port: ${PORT}`);
+})
